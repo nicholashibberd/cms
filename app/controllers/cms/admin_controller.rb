ca752b2f15140239c389401b1af8a8efbe376fcd
nicholashibberd <nicholashibberd@gmail.com>
2012-04-14 18:18:20 +0100

    moving setup to admin controller

--- app/controllers/cms/admin_controller.rb
+++ app/controllers/cms/admin_controller.rb
@@ -2,9 +2,14 @@ module Cms
   class AdminController < ApplicationController
     protect_from_forgery
   
-    before_filter :login_required
+    before_filter :login_required, :setup
   
     def login_required
+    end
+    
+    def setup
+      @group = params[:group_id] ? Group.find_by_slug(params[:group_id]) : nil
+      @group_id = @group ? @group.id : nil
     end
     
     def home
--- app/controllers/cms/cms_controller.rb
+++ app/controllers/cms/cms_controller.rb
@@ -1,11 +1,10 @@
 module Cms
-  class ApplicationController < ActionController::Base
+  class CmsController < ActionController::Base
     before_filter :setup
     
     def setup
       @group = params[:group_id] ? Group.find_by_slug(params[:group_id]) : nil
       @group_id = @group ? @group.id : nil
-      raise [params, @group, @group_id].inspect      
     end
         
   end
