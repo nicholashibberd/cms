5824f4174c5dea7c427b3b75a9a9061e42e3856a
nicholashibberd <nicholashibberd@gmail.com>
2012-05-17 20:59:03 +0100

    adding fields to photo widget

--- app/models/photo_widget.rb
+++ app/models/photo_widget.rb
@@ -1,4 +1,17 @@
 class PhotoWidget < Widget
   belongs_to :photo
+  
+  field :caption
+  field :display_in_lightbox, :default => true, :type => Boolean
+  field :width, :type => Integer
+  field :link
+  
+  def image_width
+    if panel && !panel.width.nil?
+      "#{panel.width}x>"
+    else 
+      width.nil? ? '140x>' : "#{width}x>"
+    end
+  end
 
 end
