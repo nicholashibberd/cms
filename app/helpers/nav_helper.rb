ed6844f3eac8c2fb5caed516f0cce1c65c6ba175
nicholashibberd <nicholashibberd@gmail.com>
2012-05-30 18:52:25 +0100

    editing the nav helper to cover link targets

--- app/helpers/nav_helper.rb
+++ app/helpers/nav_helper.rb
@@ -1,7 +1,7 @@
 module NavHelper
   def nav_link(nav_item)
-    link_target = nav_item.page ? host_page_path(nav_item.page) : nav_item.link_target
-    if nav_item.page == @page
+    link_target = !nav_item.link_target.blank? ? nav_item.link_target : nav_item.page ? host_page_path(nav_item.page) : '/'
+    if nav_item.page == @page || link_target == request.path
       link_to nav_item.name, link_target, :class => 'selected'
     else
       link_to nav_item.name, link_target
